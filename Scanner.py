2e2c51ae94c7006f9ac2aea1dcdbecde10aff9b9
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2022-08-24 19:12:24 +0530

    Region selector and scanning

--- Scanner.py
+++ Scanner.py
@@ -5,11 +5,11 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-imgQ = cv2.imread('Forms\\test2.jpg')
+imgQ = cv2.imread('Forms\\test1.jpg')
 
 #imgQ = cv2.resize(imgQ,(w//3,h//3))
 per = 25
-roi = [[50,60,300,50],'box','Sign']
+roi = [[(30, 22), (808, 528), 'text', 'Notice']]
 
 
 orb = cv2.ORB_create(1000)
@@ -20,6 +20,7 @@ path = 'Forms'
 myPicList = os.listdir(path)
 print(myPicList)
 for j,y in enumerate(myPicList):
+    print("idk man")
     img = cv2.imread(path + "/" + y)
     #cv2.imshow(y,img)
     kp2,des2 = orb.detectAndCompute(img,None)
@@ -37,7 +38,25 @@ for j,y in enumerate(myPicList):
     M,_ = cv2.findHomography(srcPoints,dstPoints,cv2.RANSAC,5.0)
     h, w, c = img.shape
     imgScan = cv2.warpPerspective(img,M,(w,h))
-    cv2.imshow(y, imgScan)
+    #cv2.imshow(y, imgScan)
+
+    imgShow = imgScan.copy()
+    imgMask = np.zeros_like(imgShow)
+
+    myData = []
+
+    print(f'#######Extracting Data from form {j}#######')
+
+    for x,r in enumerate(roi):
+        cv2.rectangle(imgMask,(r[0][0],r[0][1]),(r[1][0],r[1][1]),(0,255,0),cv2.FILLED)
+        imgShow = cv2.addWeighted(imgShow,0.99,imgMask,0.1,0)
+    cv2.imshow(y,imgShow)
+
+    imgCrop = imgScan[r[0][1]:r[1][1], r[0][0]:r[1][0]]
+    cv2.imshow(str(x),imgCrop)
+
+
+
 
 #cv2.imshow("KeyPointsQuery",imgKp1)
 #cv2.imshow("Output",imgQ)
