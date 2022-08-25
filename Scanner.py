a9c7f48261cc552f829557e4971e896cc2cef0b3
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2022-08-25 19:28:26 +0530

    Docx and updated for Target file

--- Scanner.py
+++ Scanner.py
@@ -2,25 +2,27 @@ import cv2
 import numpy as np
 import os
 import pytesseract
+import aspose.words as aw
+import docx
+import RegionSelector
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-imgQ = cv2.imread('Forms\\test1.jpg')
+imgQ = cv2.imread('TargetFile\\test1.jpg')
 
 #imgQ = cv2.resize(imgQ,(w//3,h//3))
 per = 25
-roi = [[(30, 22), (808, 528), 'text', 'Notice']]
+#roi = [[(30, 6), (584, 500), 'text', 'poem']]
 
 
 orb = cv2.ORB_create(1000)
 kp1,des1 = orb.detectAndCompute(imgQ,None)
 #imgKp1 = cv2.drawKeypoints(imgQ,kp1,None)
 
-path = 'Forms'
+path = 'TargetFile'
 myPicList = os.listdir(path)
 print(myPicList)
 for j,y in enumerate(myPicList):
-    print("idk man")
     img = cv2.imread(path + "/" + y)
     #cv2.imshow(y,img)
     kp2,des2 = orb.detectAndCompute(img,None)
@@ -30,7 +32,7 @@ for j,y in enumerate(myPicList):
     matches.sort(key= lambda x: x.distance)
     good = matches[:int(len(matches)*(per/100))]
     imgMatch = cv2.drawMatches(img,kp2,imgQ,kp1,good[:100],None,flags=2)
-    cv2.imshow(y,imgMatch)
+    #cv2.imshow(y,imgMatch)
 
     srcPoints = np.float32([kp2[m.queryIdx].pt for m in good]).reshape(-1,1,2)
     dstPoints = np.float32([kp2[m.queryIdx].pt for m in good]).reshape(-1,1,2)
@@ -47,6 +49,8 @@ for j,y in enumerate(myPicList):
 
     print(f'#######Extracting Data from form {j}#######')
 
+    roi = RegionSelector.function(y)
+
     for x,r in enumerate(roi):
         cv2.rectangle(imgMask,(r[0][0],r[0][1]),(r[1][0],r[1][1]),(0,255,0),cv2.FILLED)
         imgShow = cv2.addWeighted(imgShow,0.99,imgMask,0.1,0)
@@ -58,7 +62,19 @@ for j,y in enumerate(myPicList):
     print(f'{r[3]} :{pytesseract.image_to_string(imgCrop)}')
     myData.append(pytesseract.image_to_string(imgCrop))
 
+mydoc = docx.Document()
+for i in range(len(myData)):
+    mydoc.add_paragraph(myData[i])
 
+mydoc.save("my_written_file.docx")
+
+'''doc = aw.Document()
+builder = aw.DocumentBuilder(doc)
+
+for i in range(len(myData)):
+    builder.write(myData[i])
+
+doc.save(myPicList[0]+".docx")'''
 
 #cv2.imshow("KeyPointsQuery",imgKp1)
 #cv2.imshow("Output",imgQ)
